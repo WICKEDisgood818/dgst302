b80288ebc6d4d65e225ef31cbb46f592febc2466
WICKEDisgood818 <95060951+WICKEDisgood818@users.noreply.github.com>
2025-12-11 23:52:03 -0500

    Update mySketch.js

--- portfolio/rainbowGrid/mySketch.js
+++ portfolio/rainbowGrid/mySketch.js
@@ -1,16 +1,26 @@
 const diameter = 75;
 const spacing = 85;  // distance between centers 
 const gridWidth = 8; // grid width
-let purples = ["#420a5e", "#55236e", "#683b7e", "#7b548e", 
-							"#8e6c9e", "#a185af"];
-let blues = ["#1f196e", "#1c1763", "#35307d", "#4c478b", 
-						 "#625e9a", "#191458"]
+let r, g, b;
+let colors = [];
 let bg = "#000000";
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(bg); // black background
 	noLoop(); // no animation for now
+
+	r = random(0, 256);
+	g = random(0, 256);
+	b = random(0, 256);
+	colors.append([r, g, b]);
+
+	for (let i = 0; i < 4; i++) {
+		r = (r + 15) % 256;
+		g = (g + 15) % 256;
+		b = (b + 15) % 256;
+		colors.append([r, g, b]);
+	}
 }
 
 function draw() {
@@ -18,18 +28,12 @@ function draw() {
 	translate(width / 2, height / 2);
 
 	for (let y = 0; y < gridWidth; y++){
-		let colorChoice = random();
 		for (let x = 0; x < gridWidth; x++){
 			let circle_x = x * spacing - (spacing * (gridWidth - 1)) / 2;
 			let circle_y = y * spacing - (spacing * (gridWidth - 1)) / 2;
 			
-			if (colorChoice <= 0.5) {
-				stroke(random(purples));
-				fill(random(purples));
-			} else {
-				stroke(random(blues));
-				fill(random(blues));
-			}
+			stroke(random(colors));
+			fill(random(colors));
 			
 			strokeWeight(random(2, 25));
 			
