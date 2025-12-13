15144bc12361bf851d07eb05f7917cd738d1da95
WICKEDisgood818 <95060951+WICKEDisgood818@users.noreply.github.com>
2025-12-12 23:25:15 -0500

    Update mySketch.js

--- portfolio/firefighter/mySketch.js
+++ portfolio/firefighter/mySketch.js
@@ -5,10 +5,13 @@ function setup() {
 	can.position((windowWidth - width) / 2, (windowHeight - height) / 2);
 	
 	background("#84e258");
-	cursor("images/fire");
+	cursor("images/fire.png");
 }
 
 function draw() {
 	fill("#595959");
+	noStroke();
 	rect(100, 0, 100, 800);
+	rect(100, 250, 700, 100);
+	rect(500, 0, 100, 800);
 }
