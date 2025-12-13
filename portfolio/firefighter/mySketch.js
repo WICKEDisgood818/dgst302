9010aca06783e05075888cfcda2c49c4c8fc74cc
WICKEDisgood818 <95060951+WICKEDisgood818@users.noreply.github.com>
2025-12-12 23:07:33 -0500

    Update mySketch.js

--- portfolio/firefighter/mySketch.js
+++ portfolio/firefighter/mySketch.js
@@ -1,5 +1,11 @@
 function setup() {
-
+  let can = createCanvas(800, 800);
+	/* Method of canvas positioning found at this link:
+		https://github.com/processing/p5.js/wiki/Positioning-your-canvas */
+	can.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+	
+	background("#84e258");
+  
 }
 
 function draw() {
